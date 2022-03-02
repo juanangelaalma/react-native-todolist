b613cf81e74ade70c622a85e314585abb689e406
juanangelaalma <juanlah77@gmail.com>
2022-03-02 09:44:57 +0700

    add logic

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react"
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState(null)
+  const [taskItems, setTaskItems] = useState([])
+
+  const handleAddTask = () => {
+    if(!task) {
+      return;
+    }
+
+    Keyboard.dismiss()
+    setTaskItems([...taskItems, task])
+    setTask(null)
+  }
+
+  const handleDeleteTask = (index) => {
+    const itemsCopy = [...taskItems]
+    itemsCopy.splice(index, 1)
+    setTaskItems(itemsCopy)
+  }
+
   return (
     <View style={styles.container}>
 
@@ -12,11 +31,29 @@ export default function App() {
         
         <View style={styles.items}> 
           {/* This is where tasks is go */}
-          <Task text="Task 1" />
-          <Task text="Task 2" />
+          {
+            taskItems.map((item, index) => (
+              <TouchableOpacity key={index} onPress={() => handleDeleteTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            ))
+          }
         </View>
         
       </View>
+
+      {/* Write a task */}
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? 'padding' : 'height'} style={styles.writeTaskWrapper}>
+        
+        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={task => setTask(task)}></TextInput>
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -36,5 +73,33 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 30,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
   }
+
 });
